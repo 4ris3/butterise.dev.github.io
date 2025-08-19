4ec5c1925ede75071e9acf8314e7f38de216e1c4
Dawid <dawidstachowiczzz@gmail.com>
2025-08-19 16:18:33 +0200

    fix repo name

--- astro.config.production.mjs
+++ astro.config.production.mjs
@@ -5,7 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // Production configuration for GitHub Pages
 export default defineConfig({
   integrations: [tailwind()],
-  base: '/butterise.dev',
+  base: '/butterise.dev.github.io',
   outDir: './dist',
   build: {
     assets: 'assets'
