13c30fabc09032a22133fc32009779395feb58b3
Dawid <dawidstachowiczzz@gmail.com>
2025-08-19 16:41:20 +0200

    fix sub sites

--- astro.config.production.mjs
+++ astro.config.production.mjs
@@ -5,6 +5,7 @@ import tailwind from '@astrojs/tailwind';
 // Production configuration for GitHub Pages
 export default defineConfig({
   integrations: [tailwind()],
+  site: 'https://4ris3.github.io',
   base: '/butterise.dev.github.io',
   outDir: './dist',
   build: {
